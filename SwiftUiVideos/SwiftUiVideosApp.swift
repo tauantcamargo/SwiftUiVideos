0bd7e65f4134552ef72b487cb757106645cc6bd8
tauantcamargo <lukyhenson@gmail.com>
2023-04-03 18:14:19 -0300

    Initial Commit

---
+++ SwiftUiVideos/SwiftUiVideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiVideosApp.swift
+//  SwiftUiVideos
+//
+//  Created by Tauan Camargo on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiVideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
